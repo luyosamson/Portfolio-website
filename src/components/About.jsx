aa753c12307162e264b9b663a7189443398f3040
luyosamson <luyosamson@gmail.com>
2022-12-22 11:47:32 +0300

    Done

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b
+    from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 max-auto flex flex-col
+        justify-center w-full h-full'>
+            <div pb-8>
+                <p className='text-4xl font-bold inline border-b-4
+                border-gray-500'>About</p>
+            </div>
+
+            <p className='text-xl mt-20'>
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+               Suscipit quidem porro unde tenetur quia! Accusamus assumenda 
+               vel illo veritatis officiis sit voluptas in eveniet beatae,
+                reiciendis molestias repellat quam dolore. Perspiciatis, 
+                mollitia dignissimos numquam facere maiores et facilis possimus culpa error pariatur
+                odit rerum excepturi ducimus, quam provident eaque unde.
+            </p>
+            <br />
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Delectus quo aliquam cum? Sequi libero, corrupti qui repellendus velit aliquid. 
+                Quaerat voluptatem qui blanditiis assumenda quo id fuga, 
+                facere nihil sequi ipsa error at aut dolore et! Facere accusantium commodi nemo vel eos 
+                magnam veritatis,excepturi cum illum veniam, doloribus asperiores?
+
+
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
